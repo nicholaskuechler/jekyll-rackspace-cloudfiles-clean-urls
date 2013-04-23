1843753856abfe6637e2fbdc3946e688d986026d
Nicholas Kuechler <nkuechler@gmail.com>
2013-04-23 08:39:39 -0500

    Updated to pass flake8 code checks.

--- cloudfiles_jekyll_upload.py
+++ cloudfiles_jekyll_upload.py
@@ -6,16 +6,18 @@
 """ Jekyll at Rackspace Cloud Files with Clean URLs """
 
 import os
-import sys
 import cloudfiles
 
 # Rackspace Cloud API credentials
 USERNAME = "RACKSPACECLOUD_USERNAME"
 API_KEY = "RACKSPACECLOUD_API_KEY"
-# This is the name of your container. I create containers in the format: www.domain.com
+# The name of your container.
+# I create containers in the format: www.domain.com
 CONTAINER_NAME = "www.yourdomain.com"
 
-conn = cloudfiles.get_connection(username=USERNAME, api_key=API_KEY, serviceNet=False)
+conn = cloudfiles.get_connection(username=USERNAME,
+                                 api_key=API_KEY,
+                                 serviceNet=False)
 
 container = conn.get_container(CONTAINER_NAME)
 
@@ -24,13 +26,15 @@ for root, dirs, files in os.walk('_site'):
     for name in files:
         filename = os.path.join(root, name)
 
-        # Remove _site/ from the uploaded file name, or else our URL will be www.domain.com/_site/post-name
+        # Remove _site/ from the uploaded file name,
+        # or else our URL will be www.domain.com/_site/post-name
         upload_filename = filename.replace("_site/", "")
 
         # Upload the file to Rackspace Cloud Files
         obj = container.create_object(upload_filename)
 
-        # Check if the file contains HTML code. If so, we need to set the correct content type.
+        # Check if the file contains HTML code.
+        # If so, we need to set the correct content type.
         for line in open(filename):
             if "<!DOCTYPE html>" in line:
                 print "file %s is an HTML file!" % filename
@@ -40,4 +44,3 @@ for root, dirs, files in os.walk('_site'):
         fp = open(filename)
         print "uploading filename: %s" % upload_filename
         obj.write(fp)
-
